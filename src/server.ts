447b651758f2f8f754126890206af64ab627d8cc
Rodrigo Anselmo <69397304+Rodrigobanselmo@users.noreply.github.com>
2021-12-25 23:06:37 -0300

    bot created

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,36 @@
-import { v4 } from 'uuid';
-require('dotenv').config();
 import { app } from './app';
+import TelegramBot from 'node-telegram-bot-api';
 
-import { PrismaClient } from '@prisma/client';
+require('dotenv').config();
 
-const prisma = new PrismaClient();
+const token = process.env.TOKEN;
+const chatId = 1301254235;
 
-async function main() {
-  app.listen(3333, () =>
-    console.log(`Server is running on http://${process.env.HOST}:${3333}/`),
-  );
-}
+const message = `
+rodrigo
+<b>bold</b> <strong>bold</strong>
+<i>italic</i>, <em>italic</em>
+<u>underline</u>, <ins>underline</ins>
+<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+<b>bold <i>italic bold <s>italic bold strikethrough</s> <u>underline italic bold</u></i> bold</b>
+<a href="http://www.example.com/">inline URL</a>
+<a href="tg://user?id=1301254235">inline mention of a user</a>
+<code>inline fixed-width code</code>
+<pre>pre-formatted fixed-width code block</pre>
+<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
+`;
 
-main()
-  .catch(e => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
+if (token) {
+  const bot = new TelegramBot(token, { polling: true });
+  bot.on('message', msg => {
+    const chatId = msg.chat.id;
+    console.log(`chatId`, chatId);
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, message, {
+      parse_mode: 'HTML',
+    });
   });
+}
+// app.listen(3333, () =>
+//   console.log(`Server is running on http://localhost:${3333}/`),
+// );
